926940b35d610c6a59b17d809847cf3346e68f43
cringe <bruh@cringe.cum>
2020-09-28 01:40:43 +0300

    lolkek

--- lab1/task6.sh
+++ lab1/task6.sh
@@ -3,6 +3,6 @@
 INFO="$(cat /var/log/anaconda/X.log | awk  '$3 == "(II)"')"
 WARNING="$(cat /var/log/anaconda/X.log | awk  '$3 == "(WW)"')"
 
+exec 1> full.log
 echo "${WARNING//"(WW)"/"WARNING "}"
 echo "${INFO//"(II)"/"INFO "}"
-
